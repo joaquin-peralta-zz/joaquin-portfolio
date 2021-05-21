702ddd428a7be651ea0bc4bb432970ac1546199a
Joaquin Peralta <joaquinperalta1406@gmail.com>
2021-05-21 12:19:29 -0300

    Added: Navbar and menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,28 +1,70 @@
+import { useState } from 'react';
 import styles from '@styles/components/Navbar.module.scss';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { MdMenu } from 'react-icons/md';
+import { MdMenu, MdClose } from 'react-icons/md';
+import Link from 'next/link';
+import { IconContext } from 'react-icons';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import items from './menuData.json';
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <header className={styles.container}>
-      <Container fluid className="h-100">
-        <Row className="h-100 align-items-center justify-content-between">
-          <Col>
-            <MdMenu style={{ fontSize: '24px', color: '#60B6F8' }} />
-          </Col>
-          <Col className="text-right">
-            <button className={styles.langButton} type="button">
-              ES
-            </button>
-            <button className={styles.langButton} type="button">
-              EN
-            </button>
-          </Col>
-        </Row>
-      </Container>
-    </header>
+    <>
+      <header className={styles.headerContainer}>
+        <Container fluid className="h-100">
+          <Row className="h-100 align-items-center justify-content-between">
+            <Col>
+              <button
+                className={styles.menuButton}
+                type="button"
+                onClick={() => setShowNav(!showNav)}
+              >
+                {!showNav && <MdMenu style={{ fontSize: '24px', color: '#60B6F8' }} />}
+                {showNav && <MdClose style={{ fontSize: '24px', color: '#60B6F8' }} />}
+              </button>
+            </Col>
+            <Col className="text-right">
+              <button className={styles.langButton} type="button">
+                ES
+              </button>
+              <button className={styles.langButton} type="button">
+                EN
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <nav className={showNav ? styles.navContainerOpen : styles.navContainer}>
+        <ul className={styles.navList}>
+          {items.map((item) => (
+            <Link key={item.title} href={item.link}>
+              <a
+                onClick={() => setShowNav(false)}
+                onKeyDown={() => setShowNav(false)}
+                role="link"
+                tabIndex={0}
+              >
+                <li>{item.title}</li>
+              </a>
+            </Link>
+          ))}
+        </ul>
+        <div className={styles.socialMedia}>
+          <IconContext.Provider value={{ size: '36px', color: '#AEB9C2' }}>
+            <a href="https://www.linkedin.com/in/joaquin-peralta-1074291ba">
+              <FaLinkedin className="mr-3" />
+            </a>
+            <a href="https://github.com/joaquin-peralta?tab=repositories">
+              <FaGithub />
+            </a>
+          </IconContext.Provider>
+        </div>
+      </nav>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,31 +14,23 @@ export default function Home() {
     <>
       <Head>
         <title>Joaquín Peralta | Portfolio</title>
-        <meta
-          name="description"
-          content="¡Hola! Soy Joaquín, web developer. Te invito a que veas mi portfolio."
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <main className={styles.mainContent}>
         <Container>
-          <p className={styles.toMonospacePrimary}>Hola, soy</p>
-          <h1>
-            Joaquín, <br />
-            <span className={styles.colorGray}>web developer.</span>
-          </h1>
-          <p className={styles.toMonospaceGray}>Freelance Frontend Developer</p>
-          <div className="mb-3">
-            <Button variant="outline-primary">Contactame!</Button>
-          </div>
-          <div>
-            <LinkButton>Ver más</LinkButton>
+          <div className={styles.mainText}>
+            <p className={styles.toMonospacePrimary}>Hola, soy</p>
+            <h1>
+              Joaquín, <br />
+              <span className={styles.colorGray}>web developer.</span>
+            </h1>
+            <p className={styles.toMonospaceGray}>Freelance Frontend Developer</p>
+            <div className="mb-3">
+              <Button variant="outline-primary">Contactame!</Button>
+            </div>
+            <div>
+              <LinkButton>Ver más</LinkButton>
+            </div>
           </div>
         </Container>
       </main>
